94c6d2d32cfb1ffd7a7749343c3a92bf288c136a
ricardor4 <rrrodriguez@uc.cl>
2024-04-03 16:29:24 +0200

    generate pairs for dc

--- differential_cryptanalysis.py
+++ differential_cryptanalysis.py
@@ -3,5 +3,8 @@ Here we want to implement the differential cryptanalysis attack of the simple SP
 We have a differential characteristic of the form (0x0, 0x6, 0x0, 0x0) -> (0x0, 0x6, 0x0, 0x6) after 3 rounds of the cipher
 with a probability of 27/1024 (around 2.6%).
 In a ideal cipher, the probability of all differential characteristics would be 1/2^16 = 1/65536 = 0.001526
+So the characteristic that we have is around 1700 times more probable than in an ideal cipher. That distinguishes the cipher from a random permutation.
 '''
+
+
 
