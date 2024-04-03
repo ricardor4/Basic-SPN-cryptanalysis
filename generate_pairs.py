fe4da9ab1ce8ea1775a8bff7993529946e334af9
ricardor4 <rrrodriguez@uc.cl>
2024-04-03 18:28:03 +0200

    generate pairs correct

--- generate_pairs.py
+++ generate_pairs.py
@@ -1,50 +1,45 @@
 from random import randint
 from basic_SPN import encrypt
 
-def generate_pairs(n_vals: int, diff: int, key: int):
-    """
-    Generate a set of n_vals pairs of plaintexts and ciphertext with XOR difference equal to diff.
-    We generate pairs of plaintext-ciphertext (m1, c1), (m2, c2) such that m2 = m1 XOR diff.
-    The plaintexts are 16-bit integers, and the ciphertexts are also 16-bit integers.
-        
-    We use the key to encrypt the plaintexts.
+# Generate n random 16-bit plaintexts
+n = 20
+plaintexts = [randint(0, 2**16-1) for _ in range(n)]
 
+# for each plaintext we generate another plaintext that is the XOR of the original plaintext with 0b0000 1011 0000 0000
+
+diff = 0b0000_1011_0000_0000
+
+
+def generate_xored_pairs(numbers:list[int], diff:int) -> list[int]:
+    """
+    Generate a list of length 2n from the input list of integers of length n, where each even index
+    contains the original integer and each odd index contains the XOR of the previous element and diff.
+    
     Parameters:
-    n_vals: int
-        The number of pairs to generate
+    numbers: list of int
+        The original list of integers.
     diff: int
-        The XOR difference between the plaintexts
-    key: int
-        The key to use for encryption
-
+        The value to XOR with each element.
+    
     Returns:
-    None. The pairs are written to a file in the format:
-    name = key[0:20] + '.dat'
-    The file will have 2 columns, plaintext and ciphertext.
-    m1, c1
-    m2, c2
-    ...
-    m_n_vals, c_n_vals
-    where m2 onwards, all plaintexts will be mi-1 XOR diff.
+    result: list of int
+        The resulting list of length 2n with original and XORed values.
     """
+    result = []
+    for num in numbers:
+        result.append(num)  # Append the original number
+        result.append(num ^ diff)  # Append the number XORed with diff
 
-    # Produce a CSV of plaintext, key value pairs for cryptanalysis
-    file_name = 'testData/' + key[0:20] + '.dat'
-    with open(file_name, "w+") as fd_w:
-        print(f'Running basic SPN cipher with key K = {key}')
+    return result
 
-        # We select random plaintexts and encrypt them
-        plaintexts = [randint(0, 65535) for _ in range(n_vals)]
+# Generate the pairs of plaintexts and store them in a file with the format: plaintext, plaintext XOR diff
+pairs = generate_xored_pairs(plaintexts, diff)
+'''
+file_path = 'testData/plaintexts.dat'
+with open(file_path, 'w') as file:
+    for i in range(0, len(pairs), 2):
+        file.write(f'{pairs[i]:04x}, {pairs[i+1]:04x}\n')
 
-        for i in plaintexts:
-            fd_w.write(f'{i:04x}, {encrypt(i, key):04x}\n')
-            fd_w.write(f'{(i ^ diff):04x}, {encrypt((i ^ diff), key):04x}\n')
-
-        print(f'Simple SPN plaintext, ciphertext CSV written to {file_name}')
-
-if __name__ == "__main__":
-
-    # Generate a random key
-    k = '0x1234567890abcdef'
-    # now generate pairs of plaintexts and ciphertexts with XOR difference 0b0000 1011 0000 0000 under the key k
-    generate_pairs(5000, int('0000101100000000', 2), k)
+'''
+for i in range(0, len(pairs), 2):
+    print(f'{pairs[i] ^ pairs[i+1]:016b}')
